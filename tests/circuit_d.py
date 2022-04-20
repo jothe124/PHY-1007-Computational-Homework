7568762fe73f0288f5660c9f5f9c7edbab551c68
Joanie Théroux <joanie.theroux.1@ulaval.ca>
2022-04-20 16:04:19 -0400

    circuit d

--- tests/circuit_d.py
+++ tests/circuit_d.py
@@ -13,12 +13,16 @@ from src.world import World
 WORLD_SHAPE = (75, 75)
 
 WIRES = [
-    Wire(start=(12, 12), stop=(12, 63), current=Current(x=0, y=1), voltage=4.5),
-    Wire(start=(12, 63), stop=(58, 63), current=Current(x=1, y=0), voltage=4.5),
-    Wire(start=(58, 63), stop=(63, 63), current=Current(x=1, y=0), voltage=-4.5),
-    Wire(start=(63, 63), stop=(63, 12), current=Current(x=0, y=-1), voltage=-4.5),
-    Wire(start=(63, 12), stop=(17, 12), current=Current(x=-1, y=0), voltage=-4.5),
-    Wire(start=(17, 12), stop=(12, 12), current=Current(x=-1, y=0), voltage=4.5),
+    Wire(start=(12, 12), stop=(12, 37), current=Current(x=0, y=1), voltage=-4.5),
+    Wire(start=(12, 37), stop=(12, 63), current=Current(x=0, y=1), voltage=4.5),
+    Wire(start=(12, 63), stop=(37, 63), current=Current(x=1, y=0), voltage=4.5),
+    Wire(start=(37, 63), stop=(50, 63), current=Current(x=1, y=0), voltage=2.25),
+    Wire(start=(50, 63), stop=(63, 63), current=Current(x=1, y=0), voltage=-2.25),
+    Wire(start=(37, 63), stop=(37, 37), current=Current(x=0, y=-1), voltage=2.25),
+    Wire(start=(37, 37), stop=(37, 12), current=Current(x=0, y=-1), voltage=-2.25),
+    Wire(start=(63, 63), stop=(63, 12), current=Current(x=0, y=-1), voltage=-2.25),
+    Wire(start=(63, 12), stop=(37, 12), current=Current(x=-1, y=0), voltage=-2.25),
+    Wire(start=(37, 12), stop=(12, 12), current=Current(x=-1, y=0), voltage= -4.5),
 ]
 
 
